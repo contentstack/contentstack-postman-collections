757c35ce6a0f2dc7e7ddcd8b41c6148da1088495
PriyadarshanKhadtale <www.priyadarshanjkhadtale@gmail.com>
2020-06-15 18:42:36 +0530

    Initial Commit

--- src/Git.js
+++ src/Git.js
@@ -10,13 +10,16 @@ class Git {
 
   Gitpull() {
     if (fs.existsSync(this.location)) {
+
       const { stderr, stdout } = spawnSync("git", ["pull"], { cwd: this.location });
+      
       console.log(stderr.toString());
       console.log(stdout.toString());
 
     } else {
       const { stderr, stdout } = spawnSync("git", 
                 ["clone", "--depth", 1, "--branch", this.branch, this.source, this.location]);
+                
       console.log(stderr.toString());
       console.log(stdout.toString());
     }
